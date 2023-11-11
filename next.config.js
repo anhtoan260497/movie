a63dffcedee03ff48c6d3a44ac0c025f4bccd02e
anhtoan260497 <anhtoan260497@gmail.com>
2023-11-12 02:46:18 +0700

    stash

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const path = require('path')
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
   swcMinify: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
